a5fab93d6ad979f7441c2888f15d1359ebd73b8d
John D'Emic <john.demic@gmail.com>
2010-06-29 16:42:49 +0000

    - Now supports JSON payloads via Jackson.
    git-svn-id: https://svn.muleforge.org/mongodb/trunk@12 d786964b-2e89-0410-94ab-b3fa578b428f

--- src/test/java/org/mule/transport/mongodb/MongoDBFunctionalTestCase.java
+++ src/test/java/org/mule/transport/mongodb/MongoDBFunctionalTestCase.java
@@ -2,14 +2,21 @@ package org.mule.transport.mongodb;
 
 import com.mongodb.DB;
 import com.mongodb.Mongo;
+import org.mule.api.context.notification.EndpointMessageNotificationListener;
+import org.mule.api.context.notification.ServerNotification;
+import org.mule.context.notification.EndpointMessageNotification;
 import org.mule.module.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class MongoDBFunctionalTestCase extends FunctionalTestCase {
+
+    CountDownLatch latch;
 
     @Override
     protected String getConfigResources() {
@@ -24,9 +31,31 @@ public class MongoDBFunctionalTestCase extends FunctionalTestCase {
         Mongo m = new Mongo("192.168.1.11", 27017);
         DB db = m.getDB("mule-mongodb");
         db.getCollection("stuff").drop();
+
+        latch = new CountDownLatch(1);
+
+        muleContext.registerListener(new EndpointMessageNotificationListener() {
+            public void onNotification(final ServerNotification notification) {
+                final EndpointMessageNotification messageNotification = (EndpointMessageNotification) notification;
+                if (messageNotification.getEndpoint().getName().equals("endpoint.mongodb.stuff")) {
+                    latch.countDown();
+                }
+            }
+        });
     }
 
-    public void testCanInsertAndRequestData() throws Exception {
+    public void testCanInsertStringAndRequestData() throws Exception {
+        MuleClient client = new MuleClient();
+        String payload = "{\"name\": \"Johnny Five\"}";
+        client.send("vm://input", payload, null);
+        assertTrue(latch.await(5, TimeUnit.SECONDS));
+        List results = (List) client.request("mongodb://stuff", 15000).getPayload();
+        assertNotNull(results);
+        Map result = (Map) results.get(0);
+        assertEquals(result.get("name"), "Johnny Five");
+    }
+
+    public void testCanInsertMapAndRequestData() throws Exception {
         MuleClient client = new MuleClient();
         Map<String, String> payload = new HashMap<String, String>();
         payload.put("name", "Johnny Five");
@@ -37,8 +66,8 @@ public class MongoDBFunctionalTestCase extends FunctionalTestCase {
         assertNotNull(results);
         Map result = (Map) results.get(0);
         assertEquals(result.get("name"), "Johnny Five");
-
     }
+
 
     public void testCanPollForData() throws Exception {
         MuleClient client = new MuleClient();
@@ -59,5 +88,5 @@ public class MongoDBFunctionalTestCase extends FunctionalTestCase {
 
     }
 
-   
+
 }
